d53e7ae53fcd3a306a1377a27731afb48107f76a
Dávid <david.gyongyosi@majin.dev>
2023-08-31 00:39:33 +0200

    initialize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorksComponent } from './components/works/works.component';
+import { TechComponent } from './components/tech/tech.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorksComponent,
+    TechComponent,
+    NavbarComponent,
+    LoaderComponent,
+    HeroComponent,
+    FeedbacksComponent,
+    ExperienceComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
