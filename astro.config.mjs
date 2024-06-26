66b89bb828f1a3cc4f26c28377ae7196a13f5fd9
Dávid <david.gyongyosi@majin.dev>
2024-06-26 22:58:40 +0200

    tailwind added and moved to dev branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://majin.dev',
+  site: 'https://majin.dev',
+  integrations: [tailwind()]
 });
